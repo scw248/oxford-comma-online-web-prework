dad1bae7eebbb821eef54ceed4bd43954c851e37
Stephen Wright <scw248@gmail.com>
2019-01-04 16:39:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,7 @@ def oxford_comma(array)
     elsif array.length == 2
       array.join(' and ')
     elsif array.length == 3
-      before_and = array.slice(0..1)
-      before_and.push("and")
+      loop
     elsif array.length > 3
       array.join(" , ")
     end
