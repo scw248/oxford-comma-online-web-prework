38a47d7b5afa3aba2f2a3f0b60f02360c4844f61
Stephen Wright <scw248@gmail.com>
2019-01-04 16:22:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   case array
     
     when array.length == 1
-      array.join
+      return array.join
     when array.length == 2
       array.join(' and ')
     when array.length == 3
