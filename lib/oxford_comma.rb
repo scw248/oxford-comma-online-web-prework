2131e7fab340f0639f2521b54ca1154aaee1f616
Stephen Wright <scw248@gmail.com>
2019-01-04 16:26:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 def oxford_comma(array)
-  case array
     
-    when .length == 1
-      return array.join
-    when .length == 2
-      return array.join(' and ')
-    when .length == 3
-      return string = array.join(" , ")
-    when .length > 3
-      return array.join(" , ")
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(' and ')
+    elsif array.length == 3
+      array.join(" , ")
+    elsif array.length > 3
+      array.join(" , ")
     end
 end
