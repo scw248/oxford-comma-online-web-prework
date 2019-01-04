ea57a9e519301bc9929b874719c764edf94345c6
Stephen Wright <scw248@gmail.com>
2019-01-04 16:53:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(' and ')
+    elsif array.length == 3
+      last = "and #{array.last}"
+      new_array = array.slice(0..-2)
+      new_array.push(last)
+      new_array.join(", ")
+    elsif array.length > 3
+      last = "and #{array.last}"
+      new_array = array.slice(0..-2)
+      new_array.push(last)
+      new_array.join(", ")
+    end
 end
