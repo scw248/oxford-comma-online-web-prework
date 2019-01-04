82550354214c1e1a7d8ca674a1397cdf8c9de894
Stephen Wright <scw248@gmail.com>
2019-01-04 16:48:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,14 +5,9 @@ def oxford_comma(array)
     elsif array.length == 2
       array.join(' and ')
     elsif array.length == 3
-      array.last
+      array.last = 
       
-      #array.each.with_index(1) do |fruit, index|
-      #if index == -1 
-       # fruit = "and #{fruit}"
-      end
-    end
-    array.join(", ")
+  
     elsif array.length > 3
       array.join(" , ")
     end
