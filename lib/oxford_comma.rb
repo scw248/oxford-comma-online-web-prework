b9c05f11f884311c4110084a81378cf8c081b709
Stephen Wright <scw248@gmail.com>
2019-01-04 16:17:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
   case array
     
+    when array.length == 1
+      array.join
+    when array.length == 2
+      
+    when array.length == 3
+    when array.length == 4
 end
